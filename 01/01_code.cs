a81fe60fc86eecfc101977ac3de7f8daaa339323
Nathan Vos <nathan.vos@greenchoice.nl>
2023-11-09 14:17:52 +0100

    onnodige comments verwijderd

--- 01/01_code.cs
+++ 01/01_code.cs
@@ -2,10 +2,8 @@
 // StreamReader reader = new StreamReader("01_demo.txt");
 StreamReader reader = new StreamReader("01_input.txt");
 
-
 //initialize Calories List
 List<int> cals = new List<int>();
-
 
 // loop over each line in the file
 int sum = 0;
@@ -40,20 +38,3 @@ Console.WriteLine(Convert.ToString(top3sum));
 // wait for input before exiting
 Console.WriteLine("Press enter to finish");
 Console.ReadLine();
-
-
-// n-athan: how do i run 01_code.cs?
-
-// GitHub Copilot: To run a C# file in Visual Studio Code, you need to follow these steps:
-
-// 1. Install the .NET Core SDK if you haven't already. You can download it from the official Microsoft website.
-// 2. Open the terminal in Visual Studio Code. You can do this by going to `View -> Terminal` or by pressing `Ctrl + ` `.
-// 3. Navigate to the directory containing your `01_code.cs` file using the `cd` command.
-// 4. Run the command `dotnet new console -o myApp` to create a new console application. This will create a new folder named `myApp` with a simple console application.
-// 5. Replace the `Program.cs` file in the `myApp` folder with your `01_code.cs` file.
-// 6. Navigate into the `myApp` directory with `cd myApp`.
-// 7. Run the command `dotnet run` to compile and run your application.
-
-// Please note that this is a general way to run C# files and might need adjustments based on your specific project setup.
-
-// For your next steps, you might want to explore how to debug your C# code in Visual Studio Code, or how to add additional C# files to your project.
