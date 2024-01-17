c4534c96f4b2eea17c800ce9b617bebd15521b59
Nathan Vos <nathan.vos@greenchoice.nl>
2024-01-17 22:17:19 +0100

    part 1

--- 15/Source/15_code.cs
+++ 15/Source/15_code.cs
@@ -1,14 +1,113 @@
 ﻿using System.Text.RegularExpressions;
 
-public class Signal {
-    public (int x, int y) position {get; set;}
-    public (int x, int y) beacon {get; set;}
-    public int distance {get; set;}
+public class Signal
+{
+    public (int x, int y) position { get; set; }
+    public (int x, int y) beacon { get; set; }
+    public int distance { get; set; }
 
-    public Signal((int x, int y) position, (int x, int y) beacon) {
+    public Signal((int x, int y) position, (int x, int y) beacon)
+    {
         this.position = position;
         this.beacon = beacon;
         this.distance = Math.Abs(position.x - beacon.x) + Math.Abs(position.y - beacon.y);
+    }
+}
+
+public class Map
+{
+    public List<List<string>> map { get; set; }
+    public int[] boundaries { get; set; }
+    public int rowLimit { get; set; }
+    public int focusRow { get; set; }
+
+    public Map(List<Signal> signals, int focusRow, int rowLimit)
+    {
+        this.boundaries = findMapBoundaries(signals, focusRow, rowLimit);
+        Console.WriteLine("map boundaries: {0}", string.Join(", ", this.boundaries));
+        Console.WriteLine("initializing map");
+        this.map = initializeMap(this.boundaries);
+        Console.WriteLine("adding signal ranges to map");
+        addSignalRangestoMap(signals);
+        Console.WriteLine("adding signals to map");
+        addSignalsToMap(signals);
+    }
+
+    public static int[] findMapBoundaries(List<Signal> signals, int focusRow, int rowLimit)
+    {
+        int minX = signals.Min(s => s.position.x - s.distance);
+        int maxX = signals.Max(s => s.position.x + s.distance);
+        int minY = focusRow - rowLimit;
+        int maxY = focusRow + rowLimit;
+        return new int[4] { minX, maxX, minY, maxY };
+    }
+
+    public bool isInRange((int x, int y) position)
+    {
+        return position.x >= this.boundaries[0] && position.x <= this.boundaries[1]
+            && position.y >= this.boundaries[2] && position.y <= this.boundaries[3];
+    }
+
+    public static List<List<string>> initializeMap(int[] boundaries)
+    {
+        List<List<string>> map = new List<List<string>>();
+        for (int y = boundaries[2]; y <= boundaries[3]; y++)
+        {
+            List<string> row = new List<string>();
+            for (int x = boundaries[0]; x <= boundaries[1]; x++)
+            {
+                row.Add(".");
+            }
+            map.Add(row);
+        }
+        return map;
+    }
+
+    public void addSignalRangestoMap(List<Signal> signals)
+    {
+        foreach (Signal signal in signals)
+        {
+            int y = signal.position.y;
+            int x = signal.position.x - this.boundaries[0];
+            for (int i = y - signal.distance; i <= y + signal.distance; i++)
+            {
+                if (i >= this.boundaries[2] && i <= this.boundaries[3])
+                {
+                    int offset = signal.distance - Math.Abs(i - y);
+                    for (int j = x - offset; j <= x + offset; j++)
+                    {
+                        this.map[i - this.boundaries[2]][j] = "#";
+                    }
+                }
+            }
+        }
+    }
+
+    public void addSignalsToMap(List<Signal> signals)
+    {
+        foreach (Signal signal in signals)
+        {
+            if (isInRange(signal.position))
+            {
+                this.map[signal.position.y - this.boundaries[2]][signal.position.x - this.boundaries[0]] = "S";
+            }
+            if (isInRange(signal.beacon))
+            {
+                this.map[signal.beacon.y - this.boundaries[2]][signal.beacon.x - this.boundaries[0]] = "B";
+            }
+        }
+    }
+
+    public void drawMap()
+    {
+        foreach (List<string> row in this.map)
+        {
+            foreach (string sign in row)
+            {
+                Console.Write(sign);
+            }
+            Console.WriteLine();
+        }
     }
 }
 
@@ -17,7 +116,7 @@ public class Program
     public static List<Signal> readInput(string[] lines)
     {
         List<Signal> signals = new List<Signal>();
-        string pattern = @"[x|y]=(-?\d+)"; 
+        string pattern = @"[x|y]=(-?\d+)";
 
         foreach (string line in lines)
         {
@@ -37,70 +136,35 @@ public class Program
     public static void Main(string[] args)
     {
         string file = "15_demo.txt";
-        if (args.Length == 0) {
+        if (args.Length == 0)
+        {
             Console.WriteLine("No input file specified, running demo input: 15_demo.txt");
-        } else {
+        }
+        else
+        {
             file = args[0];
         }
-        // initialize variables
-        List<List<string>> map = new List<List<string>>();
 
         // read the input file
         string[] lines = File.ReadAllLines(file);
         List<Signal> signals = readInput(lines);
         Console.WriteLine("Number of signals: {0}", signals.Count);
 
-        // find the boundaries of the map
-        int minX = signals.Min(s => s.position.x - s.distance);
-        int maxX = signals.Max(s => s.position.x + s.distance);
-        int minY = signals.Min(s => s.position.y - s.distance);
-        int maxY = signals.Max(s => s.position.y + s.distance);
-        Console.WriteLine("Map boundaries: {0}, {1}, {2}, {3}", minX, maxX, minY, maxY);
+        // signals that have a beacon further away then their y distance to 10. 
+        // their 'no beacon' ranges overlap with the mystery row
+        int mysteryRow = file == "15_demo.txt" ? 10 : 2000000;
+        List<Signal> signalsFiltered = signals.Where(s => Math.Abs(mysteryRow - s.position.y) <= s.distance).ToList();
+        Console.WriteLine("Number of signals near mystery row: {0}", signalsFiltered.Count);
 
-        // initialize the map with the right amount of rows. 
-        // map[0] is the row with y = minY
-        for (int y = minY; y <= maxY; y++) {
-            List<string> row = new List<string>();
-            for (int x = minX; x <= maxX; x++) {
-                row.Add(".");
-            }
-            map.Add(row);
-        }
-
-        // add the signalranges to the map
-        foreach (Signal s in signals){
-            int y = s.position.y;
-            int x = s.position.x-minX;
-            for (int i = y-s.distance; i <= y+s.distance; i++) {
-                int offset = s.distance - Math.Abs(i-y);
-                for (int j = x-offset; j <= x+offset; j++) {
-                    map[i-minY][j] = "#";
-                }
-            }
-        }
-
-        // add the sensors and beacons to the map
-        foreach (Signal s in signals){
-            map[s.position.y-minY][s.position.x-minX] = "S";
-            map[s.beacon.y-minY][s.beacon.x-minX] = "B";
-        }
-
-        //draw the map
-        if (file == "15_demo.txt") {
-            foreach (List<string> row in map) {
-                foreach (string sign in row) {                
-                    Console.Write(sign);
-                }
-                Console.WriteLine();
-            }
-        }
+        // create a map with the signals
+        Map map = new Map(signalsFiltered, mysteryRow, 1);
+        if (file == "15_demo.txt") { map.drawMap(); }
 
         // count the number of points in the specified row that are in the signalranges
-        int index = file == "15_demo.txt" ? 10-minY : 2000000-minY;
-        int count = map[index].Count(s => s == "#");
+        int index = mysteryRow - map.boundaries[2];
+        int count = map.map[index].Count(s => s == "#");
 
         Console.WriteLine("Total score part 1: {0}", count);
-        // Console.WriteLine("Total score part 2: {0}",);
 
 
         // wait for input before exiting
