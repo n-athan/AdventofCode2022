5fae3b43869536317664b85243f4dbc81007a838
n-athan <dump_nathan@gmail.com>
2024-01-19 22:11:10 +0100

    splitlines voor meerdere OS/dotnet versies

--- 15/Tests/UnitTest1.cs
+++ 15/Tests/UnitTest1.cs
@@ -2,7 +2,7 @@ namespace Tests;
 
 public class Tests
 {
-    public static string[] Lines = @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    public static string demo = @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
 Sensor at x=12, y=14: closest beacon is at x=10, y=16
@@ -15,7 +15,9 @@ Sensor at x=20, y=14: closest beacon is at x=25, y=17
 Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3".Split("\r\n");
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+
+    public static string[] Lines = demo.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 
     public static List<Signal> Signals = Program.readInput(Lines);
 
