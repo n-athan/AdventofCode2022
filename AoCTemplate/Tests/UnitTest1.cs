84e765e70f74534b7731f441ec07450e215f4f26
n-athan <dump_nathan@gmail.com>
2024-01-20 00:07:12 +0100

    restructure for use of solutions

--- AoCTemplate/Tests/UnitTest1.cs
+++ AoCTemplate/Tests/UnitTest1.cs
@@ -2,14 +2,31 @@ namespace Tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+    public static string demo = @"
+";
+
+    public static string[] Lines = demo.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+    public static List<>  = Program.readInput(Lines);
+
+    [TestCase()]
+    public void TestParser()
     {
+        var lines = Lines;
+        Assert.That();
     }
 
     [Test]
-    public void Test1()
+    public void part1()
     {
-        Assert.Pass();
+        var lines = Lines;
+        Assert.That(, Is.EqualTo());
     }
+
+    // [Test]
+    // public void part2()
+    // {
+    //     var lines = Lines;
+    //     Assert.That(, Is.EqualTo());
+    // }
 }
